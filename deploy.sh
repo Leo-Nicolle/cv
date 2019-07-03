002ed390fb7b8bdfa76fb780848bcbe383d3a447
Leo Nicolle <nicolle_leo@yahoo.fr>
2019-07-03 18:30:51 +1000

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+IP=$(cat ~/Documents/crypted/ip)
+path="server/public/cv/"
+
+#read details
+echo -e "Building..."
+npm run build
+echo -n User:
+read username
+fullpath="/home/$username/$path"
+remotePath="$username@$IP:$fullpath"
+echo -e "Removing old version at $remotePath"
+ssh "$username@$IP" "rm -R $fullpath"
+echo -e "Sending to remote at $remotePath"
+
+scp -r dist/ $username@$IP:/home/$username/$path
